4daa97f71f2f710807d9bcfc6baedec63dc6fc71
r-tomiyama <peko858.ab@gmail.com>
2024-08-29 23:40:37 +0900

    add: api

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,41 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { zValidator } from "@hono/zod-validator"
+import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
+import { v4 as uuidv4 } from 'uuid';
 
 const app = new Hono()
+const prisma = new PrismaClient()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/todos', async (c) => {
+  const todos = await prisma.todo.findMany()
+  return c.json(todos)
+})
+
+app.get('/todos/:id', async (c) => {
+  const id = c.req.param('id')
+  const todo = await prisma.todo.findUnique({ where: { id } })
+  return c.json(todo)
+})
+
+const postSchema = z.object({
+  title: z.string(),
+  description: z.string().optional(),
+})
+app.post('/todos', zValidator("json", postSchema, (result, c) => {
+  if (!result.success) {
+    return c.json({ message: "bad request" }, 400);
+  }
+}), async (c) => {
+  const input = c.req.valid("json");
+  const todo = await prisma.todo.create({ data: {
+    id: uuidv4(),
+    title: input.title,
+    description: input.description,
+    isCompleted: false
+  } })
+  return c.json(todo)
 })
 
 const port = 3000
