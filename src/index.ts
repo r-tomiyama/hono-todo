b6c46de5f325656cef12d12cd7040f9497732a91
r-tomiyama <peko858.ab@gmail.com>
2024-08-30 00:04:30 +0900

    add: count api

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { zValidator } from "@hono/zod-validator"
 import { PrismaClient } from '@prisma/client'
+import { countByTitle, countByIsCompleted, test } from '@prisma/client/sql'
 import { z } from 'zod'
 import { v4 as uuidv4 } from 'uuid';
 
@@ -17,6 +18,12 @@ app.get('/todos/:id', async (c) => {
   const id = c.req.param('id')
   const todo = await prisma.todo.findUnique({ where: { id } })
   return c.json(todo)
+})
+
+app.get('/todos/completed/count', async (c) => {
+  const res = await prisma.$queryRawTyped(countByIsCompleted(true))
+
+  return c.json({ count: res[0].count || 0 })
 })
 
 const postSchema = z.object({
