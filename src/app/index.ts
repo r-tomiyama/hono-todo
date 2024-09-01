5260e5e6a2138ff2e8ae68d80b8507e26b8e6f13
r-tomiyama <peko858.ab@gmail.com>
2024-09-01 13:24:02 +0900

    clean: lint

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,48 +1,53 @@
+import { zValidator } from "@hono/zod-validator";
+import { PrismaClient } from "@prisma/client";
+import { countByIsCompleted } from "@prisma/client/sql";
+import { Hono } from "hono";
+import { v4 as uuidv4 } from "uuid";
+import { z } from "zod";
 
-import { Hono } from 'hono'
-import { zValidator } from "@hono/zod-validator"
-import { PrismaClient } from '@prisma/client'
-import { countByIsCompleted } from '@prisma/client/sql'
-import { z } from 'zod'
-import { v4 as uuidv4 } from 'uuid';
+const app = new Hono();
+const prisma = new PrismaClient();
 
-const app = new Hono()
-const prisma = new PrismaClient()
+app.get("/todos", async (c) => {
+	const todos = await prisma.todo.findMany();
+	return c.json(todos);
+});
 
-app.get('/todos', async (c) => {
-  const todos = await prisma.todo.findMany()
-  return c.json(todos)
-})
+app.get("/todos/:id", async (c) => {
+	const id = c.req.param("id");
+	const todo = await prisma.todo.findUnique({ where: { id } });
+	return c.json(todo);
+});
 
-app.get('/todos/:id', async (c) => {
-  const id = c.req.param('id')
-  const todo = await prisma.todo.findUnique({ where: { id } })
-  return c.json(todo)
-})
+app.get("/todos/completed/count", async (c) => {
+	const res = await prisma.$queryRawTyped(countByIsCompleted(true));
 
-app.get('/todos/completed/count', async (c) => {
-  const res = await prisma.$queryRawTyped(countByIsCompleted(true))
-
-  return c.json({ count: res[0].count || 0 })
-})
+	return c.json({ count: res[0].count || 0 });
+});
 
 const postSchema = z.object({
-  title: z.string(),
-  description: z.string().optional(),
-})
-app.post('/todos', zValidator("json", postSchema, (result, c) => {
-  if (!result.success) {
-    return c.json({ message: "bad request" }, 400);
-  }
-}), async (c) => {
-  const input = c.req.valid("json");
-  const todo = await prisma.todo.create({ data: {
-    id: uuidv4(),
-    title: input.title,
-    description: input.description,
-    isCompleted: false
-  } })
-  return c.json(todo)
-})
+	title: z.string(),
+	description: z.string().optional(),
+});
+app.post(
+	"/todos",
+	zValidator("json", postSchema, (result, c) => {
+		if (!result.success) {
+			return c.json({ message: "bad request" }, 400);
+		}
+	}),
+	async (c) => {
+		const input = c.req.valid("json");
+		const todo = await prisma.todo.create({
+			data: {
+				id: uuidv4(),
+				title: input.title,
+				description: input.description,
+				isCompleted: false,
+			},
+		});
+		return c.json(todo);
+	},
+);
 
-export default app
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { serve } from '@hono/node-server'
-import app from './app'
+import { serve } from "@hono/node-server";
+import app from "./app";
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
+const port = 3000;
+console.log(`Server is running on port ${port}`);
 
 serve({
-  fetch: app.fetch,
-  port
-})
+	fetch: app.fetch,
+	port,
+});
